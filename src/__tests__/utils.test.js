01c57e893d94285af5b8639324a359e2f2ef850b
Manny <mhfernan35277@gmail.com>
2024-03-09 00:40:06 -0700

    adds file

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,12 @@
 // Your tests here
+import { pointsForWord } from '../utils'
+
+describe("pointsForWord", () => {
+    it("calculates total points for a word (1 point for a vowel, 2 for a consonant)", () => {
+        const word = "test"
+        const points = pointsForWord(word)
+        expect(points).toBe(7)
+    })
+
+    
+})
